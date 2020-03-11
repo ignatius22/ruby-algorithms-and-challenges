3407652139a8d4d967a2b103cd5605de78d478d7
zayneio <hello@zayne.io>
2020-03-10 23:15:06 -0400

    array nth item

--- array_nth_item.rb
+++ array_nth_item.rb
@@ -1,4 +1,6 @@
-# Challenge: given an array and number n, return an array that only contains every nth record
+# Challenge: given an array and number (n), return an array that only contains every nth item
+
+# using each_slice + map
 def nth_items_1(array, n)
   array.each_slice(n).map(&:last)
 end
@@ -7,7 +9,7 @@ p nth_items_1([*1..100], 11)
 # [11, 22, 33, 44, 55, 66, 77, 88, 99, 100]
 
 
-# Challenge: given an array and number n, return an array that only contains every nth record
+# using select + with_index
 def nth_items_2(array, n)
   array.select.with_index { |_, i| (i + 1) % n == 0 }
 end
